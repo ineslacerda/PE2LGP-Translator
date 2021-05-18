8ccbdbd407ad173ab67664de499ad289f45e1818
ineslacerda <ines.lacerda98@gmail.com>
2021-05-18 21:09:28 +0100

    change e and i visemes

--- PE2LGP/Modulo_tradutor/separate_syllables.py
+++ PE2LGP/Modulo_tradutor/separate_syllables.py
@@ -40,7 +40,7 @@ class char_line():
 class silabizer():
     def __init__(self):
         self.grammar = []
-        self.mapping = {'a': 'A', 'i': 'A', 'e': 'E', 'o': 'O', 'u': 'U', 'b': 'B', 'm': 'B', 'p': 'B', 'f': 'F', 'v': 'F'}
+        self.mapping = {'a': 'A', 'i': 'E', 'e': 'A', 'o': 'O', 'u': 'U', 'b': 'B', 'm': 'B', 'p': 'B', 'f': 'F', 'v': 'F'}
         
     def split(self, chars):
         rules  = [('cccc',2), ('xcc',1), ('ccx',2), ('csc',2), ('xc',1), ('cc',1), ('vcc',2), ('sc',1), ('cs',1), ('vc',1), ('vs',1)]
