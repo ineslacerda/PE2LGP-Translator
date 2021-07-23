daa0f5c66c90619a998c04ef86bece782cf2a1eb
ineslacerda <ines.lacerda98@gmail.com>
2021-07-23 18:39:39 +0100

    add gestos_compostos

--- PE2LGP/Modulo_tradutor/server.py
+++ PE2LGP/Modulo_tradutor/server.py
@@ -80,7 +80,8 @@ async def post_handler(request):
     try:
         translated_sentence = translate_sentence(freeling_model, palavras_glosas, freq_dic, post_data, negativa_irregular, gestos_compostos)
         print(translated_sentence)
-        translated_sentence = {'glosas': ['TRABALHO', 'MULHER', 'IRMÃO', 'MEU', 'AJUDAR_EU'], 'gestos_compostos': [False, True, False, True, False, False], 'pausas': ['false', 'false', 'false', 'false', 'false', 'frase'], 'adv_cond': [False, False, False, False, False, False], 'adv_intensidade': ['false', 'false', 'false', 'false', 'false', 'false']}
+
+        translated_sentence = {'glosas': ['VERMELHA', 'MELANCIA', 'MARIANA', 'GOSTAR', 'NÃO'], 'gestos_compostos': [False, True, False, False, False], 'pausas': ['false', 'false', 'false', 'false', 'frase'], 'adv_cond': [False, False, False, False, False], 'adv_intensidade': ['false', 'false', 'false', 'false', 'false']}
     except IndexError as err:
         print('Error translating sentence')
         translated_sentence = "Erro"
