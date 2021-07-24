c3bf709fe0d7107cee316e1e761002f056d0700d
ineslacerda <ines.lacerda98@gmail.com>
2021-07-24 22:15:57 +0100

    alterei frase

--- PE2LGP/Modulo_tradutor/server.py
+++ PE2LGP/Modulo_tradutor/server.py
@@ -81,7 +81,7 @@ async def post_handler(request):
         translated_sentence = translate_sentence(freeling_model, palavras_glosas, freq_dic, post_data, negativa_irregular, gestos_compostos)
         print(translated_sentence)
 
-        translated_sentence = {'glosas': ['RAPAZ', 'AQUELE', 'ESCREVER', 'NÃO_SABER', 'MAS', 'ELE', 'SABER', 'LER'], 'gestos_compostos': [False, False, False, False, False, False, False, False], 'pausas': ['false', 'false', 'false', 'oracao', 'false', 'false', 'false', 'frase'], 'adv_cond': [False, False, False, False, False, False, False, False], 'adv_intensidade': ['false', 'false', 'false', 'false', 'false', 'false', 'false', 'false']}
+        translated_sentence = {'glosas': ['RAPAZ', 'AQUELE', 'ESCREVER', 'NÃO_SABER', 'MAS', 'ELE', 'SABER', 'LER'], 'gestos_compostos': [False, False, False, False, False, False, False, False], 'pausas': ['false', 'false', 'false', 'oracao', 'false', 'false', 'false', 'frase'], 'adv_cond': [False, False, False, False, False, False, False, False], 'adv_intensidade': ['false', 'false', 'false', 'false', 'false', 'false', 'false', 'false'], 'exprFaciais': {'3-4': ['negativa_headshake'], '0-4': ['olhos_franzidos']}}
     except IndexError as err:
         print('Error translating sentence')
         translated_sentence = "Erro"
